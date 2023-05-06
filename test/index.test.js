73fb39d3d783896e03fb4783432d13a27de90cf0
calogero93 <40425918+calogero93@users.noreply.github.com>
2023-05-06 09:28:33 +0200

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = require('../app');
 
 
+
 describe('Index Page', function() {
   it("renders successfully", function(done) {
     request(app).get('/').expect(200, done);    
